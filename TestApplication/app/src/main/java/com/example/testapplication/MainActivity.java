14e8393b9dbc8da8b184af8b10a18a508dc84292
Saurabh-7552 <sk0497552@gmail.com>
2020-06-08 22:53:51 +0530

    Added TestApplication/

---
+++ TestApplication/app/src/main/java/com/example/testapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.testapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void MapAddress(View view) {
+     view.animate();
+
+    }
+}
